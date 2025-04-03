fb51f831d36f85224bab3d30d3f94181d2e8fd94
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2025-04-03 19:41:45 -0400

    added supabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@components/global.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,3 +30,4 @@ export default function RootLayout({
     </html>
   );
 }
+
